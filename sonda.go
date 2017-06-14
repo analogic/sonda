f76072bde3dff371ce45fe77089308b5963c54cd
SH <sh@analogic.cz>
2017-06-14 12:38:20 +0200

    change method

--- sonda.go
+++ sonda.go
@@ -15,16 +15,14 @@ import (
 	"syscall"
 	"os/exec"
 	"log"
-	"math"
 )
 
 var speedPulsesCounter int
 var directionPulsesCounter int
-var threeSecSpeedPulsesCounter int
-var direction float32
+var direction int
 
 var speeds []float32
-var directions []float32
+var directions []int
 
 func main() {
 //	runtime.GOMAXPROCS(4)
@@ -50,7 +48,6 @@ func main() {
 
 	speedPulsesCounter = 0
 	directionPulsesCounter = 0
-	threeSecSpeedPulsesCounter = 0
 
 	for p := range filteredPulsesByLogic {
 
@@ -64,21 +61,16 @@ func main() {
 			directionPulsesCounter++
 		} else {
 			speedPulsesCounter++
-			threeSecSpeedPulsesCounter++
 		}
 
 		if speedPulsesCounter == 36 {
-			newDirection := float32(((directionPulsesCounter * 10) + 70 + 180) % 360)
+			newDirection := ((directionPulsesCounter * 10) + 70 + 180) % 360
 
 			////if (newDirection == 80 || newDirection == 60 || newDirection == 70) && direction > 140 {
 				// do nothing, anemometr bug
 			////} else {
 				direction = newDirection
 			///}
-
-			/// speedPulsesCounter = 0
-			/// directionPulsesCounter = 0
-			fmt.Println("")
 		}
 	}
 }
@@ -86,20 +78,20 @@ func main() {
 func printResults(w *sonda.WebServer) {
 	counter := 0
 	for {
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 1)
 
-		speed := (float32(threeSecSpeedPulsesCounter) * (float32(30) / float32(1534))) / 3
-		direction = float32(math.Remainder(float64((float32(directionPulsesCounter) / 3 * float32(10)) + float32(70) + float32(180)), float64(360)))
+		speed := (float32(speedPulsesCounter) * (float32(30) / float32(1500)))
 		fmt.Printf("\n\033[1;34m%vm/s, %v°\033[0m\n", speed, direction)
 
 		speeds = append(speeds, speed)
 		directions = append(directions, direction)
 
 		w.WebSocket <- fmt.Sprintf("{\"direction_current\": %v, \"speed_current\": %v}", direction, speed)
-		threeSecSpeedPulsesCounter = 0
+
+		speedPulsesCounter = 0
 		directionPulsesCounter = 0
 
-		if(counter == 20) {
+		if(counter == 60) {
 			printAverages(w)
 			counter = 0
 		}
@@ -119,7 +111,7 @@ func printAverages(w *sonda.WebServer) {
 	fmt.Print(w.DataJson)
 
 	speeds = []float32{}
-	directions = []float32{}
+	directions = []int{}
 }
 
 func raspiLoad() float64 {
